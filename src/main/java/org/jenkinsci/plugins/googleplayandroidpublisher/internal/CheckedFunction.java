092d09f565545dee2447bbb3d4e70cfbcc81315d
Christopher Orr <chris@orr.me.uk>
2021-01-18 01:33:41 +0100

    Remove redundant file.

--- src/main/java/org/jenkinsci/plugins/googleplayandroidpublisher/internal/CheckedFunction.java
+++
@@ -1,8 +0,0 @@
-package org.jenkinsci.plugins.googleplayandroidpublisher.internal;
-
-import java.io.IOException;
-
-@FunctionalInterface
-public interface CheckedFunction<T, R> {
-    R apply(T t) throws IOException;
-}
